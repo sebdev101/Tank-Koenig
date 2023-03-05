b0debca6e70f27aadc58be8bcb2cbb1995af9e01
Sebdev101 <sebdev101@gmail.com>
2023-03-05 21:37:45 +0000

    Fix: Small fixes on config files
    eslint prettier vite config vitest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
 module.exports = {
   root: true,
   env: {
@@ -6,5 +9,9 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
 }
