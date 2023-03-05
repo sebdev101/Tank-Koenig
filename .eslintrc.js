ad025adcb42aceafd8a03ba2f0067633f19a8f76
Sebdev101 <sebdev101@gmail.com>
2023-03-05 22:39:36 +0000

    fix: Eslint wrong config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
   env: {
@@ -9,9 +6,5 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
 }
